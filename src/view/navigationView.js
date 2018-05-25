f2f5eabb85ab59b2879d5e6b0f27d13a5d285a4a
MrXiongHL <2713697048@qq.com>
2018-05-25 10:00:50 +0800

    ddd

---
+++ src/view/navigationView.js
@@ -0,0 +1,28 @@
+import React,{Component} from 'react';
+import { View } from 'react-native';
+
+export default NavigationView extends Component{
+	
+	constructor(props){
+		super(props);
+		
+	}
+	
+	componentWillMount() {
+		
+	}
+
+	componentDidMount() {
+
+	}
+	
+	render() {
+		return (
+			
+			<View>
+				
+			</View>
+
+		);
+	}	
+}
